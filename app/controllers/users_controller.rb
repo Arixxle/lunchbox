88cad8086a97d99da6bab06a2b3d6cc9912ee806
Joseph Hung <arixxle@gmail.com>
2020-04-22 22:16:25 +0800

    strong parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,17 @@ class UsersController < ApplicationController
   def registration
     user = params[:user]
     if user[:password] == user[:password_confirm]
-      redirect_to "/login"
+      #新增使用者
+      clean_user = params.require(:user).permit(:email, :password, :password_confirm) #strong parameters
+      u = User.new(clean_user) #上段精簡，把整個hash塞給一個變數
+      if u.save 
+        #若成功
+        #幫登入
+      redirect_to "/"
+      else 
+        #若失敗
+      redirect_to "/sign_up"
+      end
     else
       redirect_to "/sign_up"
     end
