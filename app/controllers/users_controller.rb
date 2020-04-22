30383cb69adb1213e55e4250e575d6ad0bbf1526
Joseph Hung <arixxle@gmail.com>
2020-04-22 21:35:03 +0800

    add user login if else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
     
   end
   def registration
-    render html: "Registration"
+    user = params[:user]
+    if user[:password] == user[:password_confirm]
+      redirect_to "/login"
+    else
+      redirect_to "/sign_up"
+    end
   end
 end
