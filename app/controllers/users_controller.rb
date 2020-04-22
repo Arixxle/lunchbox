950c71ef2b588eb1b7036adb806be33e8d1803ea
Joseph Hung <arixxle@gmail.com>
2020-04-22 17:09:03 +0800

    add model user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def login
+    
+  end
+  def signup
+    
+  end
+  def registration
+    render html: "Registration"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # get '/', to: "welcome#index" 
   root "welcome#index"
+  get '/login', to: "users#login"
+  get '/sign_up', to: "users#signup"
+  post '/sign_up', to: "users#registration"
 end
