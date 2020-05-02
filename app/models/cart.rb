040775b409f2ce58ba5f847a79bdad84e22e04da
Joseph Hung <arixxle@gmail.com>
2020-05-02 17:35:40 +0800

    issue #9 基本功能2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,12 @@ class Cart
     @items = [] #要有一個可以裝東西的容器
   end
   def add_item(item_id)
-    @items << item_id
+    found_item = @items.find { |item| item.item_id == item_id }
+    if found_item
+      found_item.increment! #去掉小括號，增加驚嘆好(表你要帶參數，若沒有參數，就會帶用預設值)
+    else
+      @items << CartItem.new(item_id) #去掉quantity，沒給就會帶預設值
+    end
   end
   def empty?
     @items.empty? #陣列本身就有empty?方法
